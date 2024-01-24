88f0930d659ed0b6ad6567ad0aa059b9c24d0e8c
afoo <i@afoo.me>
2024-01-24 23:03:43 +0800

    change astro build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,10 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jiagoubaike.com',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  trailingSlash: 'never',
+  build: {
+    // Example: Generate `page.html` instead of `page/index.html` during build.
+    format: 'file'
+  }
 });
