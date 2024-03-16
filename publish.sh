175a7968df4c8f8ba0110159fc5dd157f8e6eeb0
afoo <i@afoo.me>
2024-03-16 18:15:12 +0800

    add footer

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
+##### THIS IS NOT NEEDED, SINCE WE PUBLISH CONTENT TO CLOUDFLARE PAGES INSTEAD OF GITHUB PAGES #####
+##### JUST GIT PUSH TO GITHUB, CF PAGES WILL PULL AND DEPLOY #####
 currentDir=$(basename "$PWD")
 publishRepoDir="$currentDir-pages"
 
